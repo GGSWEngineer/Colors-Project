bd097ecde248ac302ad3c22b7b8fc871bf304125
Gerardo Garcia <gerardo.xxx.garcia@gmail.com>
2023-07-31 00:11:58 -0500

    Added Snackbar and styled Select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
 import Select from "@mui/material/Select";
+import { Snackbar } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import IconButton from "@mui/material/IconButton";
 import MenuItem from "@mui/material/MenuItem";
 import Slider, { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
-import "./Navbar.css"
+import "./Navbar.css";
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            format:"hex",
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event) {
-        this.setState({
-            format: event.target.value,
-        })
-        // this is the function that is passed down
-        this.props.handleChange(event.target.value);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: "hex",
+      open: false,
+    };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
+  }
+  handleFormatChange(event) {
+    this.setState({
+      format: event.target.value,
+      open:true,
+    });
+    // this is the function that is passed down
+    this.props.handleChange(event.target.value);
+  }
+  closeSnackbar() {
+    this.setState({
+      open: false,
+    });
+  }
   render() {
     const { level, changeLevel } = this.props;
     const { format } = this.state;
@@ -29,7 +40,7 @@ class Navbar extends Component {
           <a href="#">reactcolorpicker</a>
         </div>
         <div className="slider-container">
-            <span>Level: {level}</span>
+          <span>Level: {level}</span>
           <div className="slider">
             <Slider
               defaultValue={level}
@@ -41,12 +52,30 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-            <Select value={format} onChange={this.handleChange}>
-                <MenuItem value="hex">HEX - #ffffff</MenuItem>
-                <MenuItem value="rgb">RGB - rbg(255,255,255)</MenuItem>
-                <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
-            </Select>
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rbg(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed To {format.toUpperCase()}</span>}
+          ContentProps={{ "aria-describedby": "message-id" }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <ClearIcon />
+            </IconButton>,
+          ]}
+        />
       </header>
     );
   }
